47056e9b3ff4eb9a54f285dc010f18998a439fb5
Salome Naskidashvili <send.to.salome@gmail.com>
2023-07-14 18:23:26 +0400

    markdown and search quary

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:title>", views.entry_page, name="title")
+    path("<str:title>", views.entry_page, name="title"),
+    path("search/", views.search, name = "search"),
+    path("createpage/", views.create_page, name = "newpage")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 
 from . import util
+import markdown
+
+def markdown_md_to_html(entry):
+    markdowner = markdown.Markdown()
+    return markdowner.convert(util.get_entry(entry))
 
 
 def index(request):
@@ -11,5 +16,26 @@ def index(request):
 def entry_page(request, title):
     return render(request, "encyclopedia/title.html", {
         "title": title.capitalize(),
-        "entry_text": util.get_entry(title)
+        "entry_text":  markdown_md_to_html(title)
     })
+
+def search(request):
+    if request.method == "POST":
+        search_title = request.POST["q"]
+        if search_title.lower() in map(str.lower, util.list_entries()):
+            return render(request,  "encyclopedia/title.html", {
+                "title": search_title.capitalize(),
+                "entry_text":  markdown_md_to_html(search_title)
+            })
+        else:
+            return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+         })
+    else:
+        return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+         })
+    
+def create_page(request):
+    return render(request, "encyclopedia/newpage.html")
+        # util.save_entry(title, content)
